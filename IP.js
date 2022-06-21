8a3cb3d18e507e0f6204602470b7445bebb727ad
捣蛋鬼 <106107922+wei6677@users.noreply.github.com>
2022-06-21 19:20:39 +0800

    Update IP.js

--- IP.js
+++ IP.js
@@ -7,7 +7,7 @@ var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['city'];
 var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "IP地址" + ":" + obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "IP地址" + ":" + obj['org'];
 
 
 
