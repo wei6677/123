83a6ab7d531b87eff24670b2f0ce19c7f90e98be
捣蛋鬼 <106107922+wei6677@users.noreply.github.com>
2022-06-22 02:33:27 +0800

    Update 456.js

--- 456.js
+++ 456.js
@@ -4,6 +4,13 @@ if ($response.statusCode != 200) {
 
 console.log("haha")
 
+const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
+var city0 = "高谭市";
+var isp0 = "Cross-GFW.org";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function City_ValidCheck(para) {
   if(para) {
     if (para.length<7) {
