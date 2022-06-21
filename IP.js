677eea2bc188e450173c3c20afa9e5caf2da989c
捣蛋鬼 <106107922+wei6677@users.noreply.github.com>
2022-06-21 23:24:55 +0800

    Update IP.js

--- IP.js
+++ IP.js
@@ -52,7 +52,7 @@ emoji=City_ValidCheck(obj['province']) == "香港"? "🇭🇰️":emoji
 emoji=City_ValidCheck(obj['province']) == "澳门"? "️🇲🇴️":emoji
 emoji=City_ValidCheck(obj['province']) == "台湾"? "️️🇹🇼":emoji
 var title =  emoji + "" + '「'+ City_ValidCheck(obj['province'])+"」";//+Area_check(obj['country']);
-var subtitle =emoji + ""+ " ™"+ " ➠ "+ obj['country'];
+var subtitle =emoji + "+ obj['country'];
 var ip = obj['addr']; 
 var description = '服务商:'+obj['isp'] + '\n'+'定位: [' +obj["latitude"]+","+obj["longitude"]+"]"+ '\n' + 'IP:'+ obj['addr'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
