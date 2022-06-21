91366b2c5d235f0c99178c3e018bf69e990df96b
捣蛋鬼 <106107922+wei6677@users.noreply.github.com>
2022-06-22 04:23:40 +0800

    Update 123.js

--- 123.js
+++ 123.js
@@ -1,6 +1,29 @@
 if ($response.statusCode != 200) {
   $done(Null);
 }
+console.log("haha")
+const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
+var city0 = "高谭市";
+var isp0 = "Cross-GFW.org";
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function City_ValidCheck(para) {
+  if(para) {
+    if (para.length<7) {
+      return para
+    } else {
+      return para.slice(0,6)
+    }
+  
+  } else
+  {
+  return city0
+//emojis[getRandomInt(emojis.length)]
+  }
+}
+
 
 var body = $response.body;
 var obj = JSON.parse(body);
